c65aebfef554f3d5abb8f5d5d7c47bb3003d28b8
LakshiyaDhanapalan <164600806+LakshiyaDhanapalan@users.noreply.github.com>
2025-11-03 22:45:59 +0530

    Time: 10 ms (12.03%) | Memory: 47 MB (6.45%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.java
@@ -0,0 +1,83 @@
+class MyLinkedList {
+    private class Node {
+        int val;
+        Node next;
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+
+    public void addAtHead(int val) {
+        Node newnode = new Node(val);
+        newnode.next = head;
+        head = newnode;
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        Node newnode = new Node(val);
+        if (head == null) {
+            head = newnode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newnode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+        Node newnode = new Node(val);
+        Node prev = head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev.next;
+        }
+        newnode.next = prev.next;
+        prev.next = newnode;
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size || head == null) return;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            if (prev.next != null) {
+                prev.next = prev.next.next;
+            }
+        }
+        size--;
+    }
+}
